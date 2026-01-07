c6cc970d0574cff7ea71ecd9bacdaecf887759b5
emmi0122 <emiliaidajarleback@gmail.com>
2026-01-07 13:59:38 +0100

    Done with simple factory

--- simple-factory/before/Game.java
+++ simple-factory/before/Game.java
@@ -1,7 +1,13 @@
 public class Game {
   public static void main(String[] args) {
-    Troll troll = new Troll("Lillfjant", new Club());
-    Knight sirJames = new Knight("Sir James", new Sword());
-    Knight blackKnight = new Knight("Fistfighting Black Knight");
+    CharacterFactory cFactory = new SimpleCharacterFactory();
+
+    Character troll = cFactory.createCharacter(CharacterType.TROLL, WeaponType.CLUB, "Lillfjant");
+    Character sirJames = cFactory.createCharacter(CharacterType.KNIGHT, WeaponType.SWORD, "Sir James");
+    Character blackKnight = cFactory.createCharacter(CharacterType.KNIGHT, WeaponType.UNARMED, "Fistfighting Black Knight");
+
+    System.out.println(troll);
+    System.out.println(sirJames);
+    System.out.println(blackKnight);
   }
 }
